b52d8735a764b70db5b6903a421d6c1848220f7f
Ali Al-Bustami <abustami@umich.edu>
2025-10-31 23:36:13 -0400

    Isolate gum installer into utils

--- src/main.sh
+++ src/main.sh
@@ -1,21 +1,7 @@
 #!/bin/bash
 
 if ! command -v gum &> /dev/null; then
-    echo "🧩 'gum' is not installed."
-    echo "Installing gum via apt..."
-    
-    if [ "$EUID" -ne 0 ]; then
-        echo "You may be asked for your password to install gum."
-    fi
-    
-    echo "deb [trusted=yes] https://repo.charm.sh/apt/ * *" | sudo tee /etc/apt/sources.list.d/charm.list > /dev/null
-    sudo apt update && sudo apt install -y gum
-
-    # Verify installation
-    if ! command -v gum &> /dev/null; then
-        echo "❌ Gum installation failed. Please install it manually and re-run this script."
-        exit 1
-    fi
+    bash src/utils/gum_installation.sh
 fi
 
 gum style \
